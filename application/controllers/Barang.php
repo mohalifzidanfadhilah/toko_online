274dd07eaf1abbae946cbc475707fde32eab516e
mohalifzidanfadhilah <alifzidan76@gmail.com>
2021-10-11 16:51:39 +0700

    hapus,ubah,dan detail

--- application/controllers/Barang.php
+++ application/controllers/Barang.php
@@ -36,7 +36,48 @@ class Barang extends CI_controller{
             else
             {
                 $this->Barang_model->tambahDataBarang();
+                $this->session->set_flashdata('flash','Ditambah');
                 redirect('barang');
             }
     }
+
+    public function hapus($id){
+        $this->Barang_model->hapusDataBarang();
+        $this->session->set_flashdata('flash','Ditambah');
+        redirect('barang');
+    }
+
+    public function detail($id){
+        $data['judul']="Detail Barang";
+        $data['barang']=$this->Barang_model->getBarangById($id);
+        $this->load->view('templates/header',$data);
+        $this->load->view('barang/detail',$data);
+        $this->load->view('templates/footer');
+
+    }
+
+    public function Ubah($id){
+        $data['judul']="Ubah Data Barang";
+        $data['barang']=$this->Barang_model->getBarangById($id);
+
+        $this->form_validation->set_rules('id_barang', 'kode Barang', 'required');
+        $this->form_validation->set_rules('nama_barang', 'Nama Barang', 'required');
+        $this->form_validation->set_rules('harga', 'Harga', 'required|numeric');
+        $this->form_validation->set_rules('stok', 'Stok', 'required|numeric');
+
+        if ($this->form_validation->run() == FALSE)
+
+            {
+                $this->load->view('templates/header',$data);
+                $this->load->view('barang/ubah',$data);
+                $this->load->view('templates/footer');
+            }
+            else
+            {
+                $this->Barang_model->tambahDataBarang();
+                $this->session->set_flashdata('flash','Diubah');
+                redirect('barang');
+            }
+    }
+    
 }
